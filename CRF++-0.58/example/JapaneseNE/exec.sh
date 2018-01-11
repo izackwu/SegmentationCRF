8dda36ebea3f7c273e5d5bff2833d95e223f3eda
keithnull <799027522@qq.com>
2018-01-11 20:03:24 +0800

    Deleted the unnecessary doc,example,etc.

--- CRF++-0.58/example/JapaneseNE/exec.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-../../crf_learn.exe -p2 -f 3 -c 4.0 template train.data model
-../../crf_test.exe -m model test.data
-
-../../crf_learn.exe -a MIRA -f 3 template train.data model
-../../crf_test.exe -m model test.data
-rm -f model
